9252b174aa32c437af2953da26c476da3f61a021
ozdanborne <dan@projectcalico.org>
2019-02-04 19:01:54 -0800

    remove ginkgo

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -16,13 +16,10 @@ package utils
 
 import (
 	"context"
-	"fmt"
-	"os"
 	"os/exec"
 	"strings"
 
 	"github.com/kelseyhightower/envconfig"
-	. "github.com/onsi/ginkgo"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -46,15 +43,8 @@ func init() {
 
 var Ctx = context.Background()
 
-var currentTestOutput = []string{}
-
-var LastRunOutput string
-
 func Run(command string, args ...string) error {
 	outputBytes, err := Command(command, args...).CombinedOutput()
-	currentTestOutput = append(currentTestOutput, fmt.Sprintf("Command: %v %v\n", command, args))
-	currentTestOutput = append(currentTestOutput, string(outputBytes))
-	LastRunOutput = string(outputBytes)
 	if err != nil {
 		log.WithFields(log.Fields{
 			"command": command,
@@ -64,24 +54,6 @@ func Run(command string, args ...string) error {
 
 	return err
 }
-
-func AddToTestOutput(args ...string) {
-	currentTestOutput = append(currentTestOutput, args...)
-}
-
-var _ = BeforeEach(func() {
-	currentTestOutput = []string{}
-})
-
-var _ = AfterEach(func() {
-	if CurrentGinkgoTestDescription().Failed {
-		os.Stdout.WriteString("\n===== begin output from failed test =====\n")
-		for _, output := range currentTestOutput {
-			os.Stdout.WriteString(output)
-		}
-		os.Stdout.WriteString("===== end output from failed test =====\n\n")
-	}
-})
 
 func RunCommand(command string, args ...string) error {
 	cmd := Command(command, args...)
